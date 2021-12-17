90ae2212a8314c258f564847cd969a4eb3e921d9
Seisape <seisape@haverford.edu>
2021-12-17 11:04:51 -0500

    updated code and comments

--- create_presentation_county_graphs.py
+++ create_presentation_county_graphs.py
@@ -1,6 +1,6 @@
 """
 Authors:        Seun Eisape & Victor Nault
-Description:    This file creates the two deleware DSCI graphs used in our
+Description:    This file creates the two Delaware DSCI graphs used in our
                 presentation
                 presentation
 Date:           12/17/21
@@ -10,15 +10,14 @@ import json
 
 def main():
     """
-    This function creates DSCI graphs for the counties of Deleware
+    This function creates DSCI graphs for the counties of Delaware
     """
     all_counties_dict = json.load(open("all_counties.json"))
     for county in all_counties_dict:
         if county[0:2] == "DE":
-            #print(all_counties_dict[county])
-            DE_county = DsciDataset(all_counties_dict[county], int(len(all_counties_dict[county])/2), county)
+            DE_county = DsciDataset(all_counties_dict[county], \
+            int(len(all_counties_dict[county])/2), county)
             DE_county.export_prediction_plot()
-            #print(DE_county)
     pass
 
 if __name__ == '__main__':
