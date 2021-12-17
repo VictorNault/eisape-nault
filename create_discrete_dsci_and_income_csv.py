d6564116f51f2f15cb8e2dde6ba994f2a82aadb6
vnault <vnault@haverford.edu>
2021-12-17 11:02:26 -0500

    Commented

--- create_discrete_dsci_and_income_csv.py
+++ create_discrete_dsci_and_income_csv.py
@@ -1,40 +1,57 @@
 """
 Authors:        Seun Eisape & Victor Nault
-Description:    This file
-Date:
+Description:    This script creates a .csv file containing the discretized
+                DSCI data and income data for the counties in
+                counties_2019.json and med_income_dict. The DSCI data is
+                discretized by averaging the average DSCIs for each county,
+                then if the average for any given county is at or above this
+                value, it is classifed as "Above Average", and if it is below,
+                "Below Average". Then, if the median income, as found in
+                med_income_dict, is at or above the low income cutoff, it is
+                classified as "Not low income", and if it is below, "Low
+                income". Finally these values are written to "naive_bayes.csv".
+Date: 12/17/21
 """
+
 import json
 import csv
 
 def main():
     """
-    This function
+    This function loads the dictionaries found in the .json files containing
+    median income and average DSCI for every county, then passes them to
+    helper functions to discretize them, then passes the discretized values to
+    a helper function to export them.
     """
     counties_2019 = json.load(open("counties_2019.json"))
     discrete_dsci_dict = discretize_dsci_dict(counties_2019)
-    #print(discrete_dsci_dict)
     med_income_dict = json.load(open("income_dict.json"))
     discrete_med_income_dict = discretize_med_income_dict(med_income_dict)
-    #print(discrete_med_income_dict)
     create_csv(discrete_dsci_dict, discrete_med_income_dict)
 
 def discretize_dsci_dict(dsci_dict):
     """
-    This function
+    This function discretizes the continuous average DSCI data by averaging the
+    average DSCIs for each county, then if the average for any given county is
+    at or above this value, classifying it as "Above Average", and if it is
+    below, "Below Average". Each value is linked to its county via a dictionary.
+    Parameter:  A dictionary with states as keys to dictionaries with counties
+    as keys and average DSCI (single numerical value) as values.
+    Returns: A dictionary with the same keys and structure as the parameter but
+    discretized values.
     """
 
     avg_dsci = 0
     county_counter = 0
     for state in dsci_dict:
-        for county in dsci_dict[state]:
+        for county in dsci_dict[state]:averaging
+    said median income for each count, then
             avg_dsci += dsci_dict[state][county]
             county_counter += 1
     avg_dsci = avg_dsci / county_counter
-    #print(avg_dsci)
     discrete_dsci_dict = {}
     for state in dsci_dict:
         this_state_county_dict = {}
-        #discrete_dsci_dict[state]
         for county in dsci_dict[state]:
             if (dsci_dict[state][county] >= avg_dsci):
                 this_state_county_dict[county] = "Above average DSCI"
@@ -45,9 +62,19 @@ def discretize_dsci_dict(dsci_dict):
 
 def discretize_med_income_dict(med_income_dict):
     """
-    This function
+    This function discretizes the continuous median income data by comparing
+    the median income of the county to a hardcoded cutoff value. If the median
+    income is at or above the low income cutoff, it is classified as "Not low
+    income", and if it is below, "Low income". Each value is linked to its
+    county via a dictionary.
+    Parameter:  A dictionary with counties as keys and median income (single
+    numerical value) as values.
+    Returns: A dictionary with the same keys as the parameter but discretized
+    values.
     """
 
+    # based on the National Center for Children in Poverty definition of low
+    # income as below $51852 a year for a family of three
     low_income_cutoff = 51852
     discrete_med_income_dict = {}
     for county in med_income_dict:
@@ -59,9 +86,15 @@ def discretize_med_income_dict(med_income_dict):
 
 def create_csv(dsci_dict, med_income_dict):
     """
-    This function
+    This function writes all the DSCI values in a dictionary of states to
+    counties to DSCI values, and all the median income values in a dictionary
+    of counties to median incomes, to a .csv file, where the columns are these
+    two values and the rows are each county.
+    Parameters: dsci_dict - A dictionary with counties as keys and median income
+                (single numerical value) as values.
+                med_income_dict - A dictionary with counties as keys and median
+                income (single numerical value) as values.
     """
-    
     with open("naive_bayes.csv", "w", newline="") as naive_bayes_csv:
         headers = ["DSCI", "Income"]
         writer = csv.writer(naive_bayes_csv)
@@ -69,12 +102,12 @@ def create_csv(dsci_dict, med_income_dict):
         for state in dsci_dict:
             for county in dsci_dict[state]:
                 try:
-                    writer.writerow([dsci_dict[state][county], med_income_dict[county]])
+                    writer.writerow([dsci_dict[state][county], \
+                    med_income_dict[county]])
+                # If a county exists in the dsci_dict but not the
+                # med_income_dict, ignore it
                 except KeyError:
                     pass
-
-
-
 
 if __name__ == "__main__":
     main()
